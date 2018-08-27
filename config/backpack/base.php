e282e9d684dd4052a833d4b376ed24192c2a4eb4
MustafaWIthA <mustafa.abdsh@gmail.com>
2018-08-28 01:31:29 +0300

    do again

--- config/backpack/base.php
+++ config/backpack/base.php
@@ -12,17 +12,17 @@ return [
     */
 
     // Project name. Shown in the breadcrumbs and a few other places.
-    'project_name' => 'Backpack',
+    'project_name' => 'NRC App',
 
     // Menu logos
     'logo_lg'   => '<b>Back</b>pack',
     'logo_mini' => '<b>B</b>p',
 
     // Developer or company name. Shown in footer.
-    'developer_name' => 'Cristian Tabacitu',
+    'developer_name' => 'M',
 
     // Developer website. Link in footer.
-    'developer_link' => 'http://tabacitu.ro',
+    'developer_link' => 'Ilsoftare.so',
 
     // Show powered by Laravel Backpack in the footer?
     'show_powered_by' => true,
